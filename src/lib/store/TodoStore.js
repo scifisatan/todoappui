79b5dd8b0648f417e6b8269d201af1c39498c139
scifisatan <brawlershere@gmail.com>
2023-04-07 12:05:46 +0545

    ok

--- src/lib/store/TodoStore.js
+++ src/lib/store/TodoStore.js
@@ -1,57 +1,47 @@
 import { v4 as uuidv4 } from 'uuid';
-import {writable} from 'svelte/store';
+import { writable } from 'svelte/store';
 import { browser } from '$app/environment';
 
-const data = browser ? JSON.parse(window.localStorage.getItem('st-todo-list')?? []) : [];
+const data = browser ? JSON.parse(window.localStorage.getItem('st-todo-list')) ?? [] : [];
 
 export const todos = writable(data);
 
 todos.subscribe((value) => {
-        
-    if (browser) {
-        localStorage.setItem('st-todo-list', JSON.stringify(value));
-    }
+	if (browser) {
+		localStorage.setItem('st-todo-list', JSON.stringify(value));
+	}
 });
 
-export const addTodo = (todo) => {
-    todos.update((currentTodos) => {
-        return [...currentTodos,{id: uuidv4(), text: '', complete: false}];
-    });
-}
+export const addTodo = () => {
+	todos.update((currentTodos) => {
+		return [...currentTodos, { id: uuidv4(), text: '', complete: false }];
+	});
+};
 
-//deleteTodo function
 export const deleteTodo = (id) => {
-    todos.update((currentTodos) => {
-        return currentTodos.filter((todo) => todo.id !== id);
-    });
-}
+	todos.update((currentTodos) => {
+		return currentTodos.filter((todo) => todo.id !== id);
+	});
+};
 
-//toggleTodo function
-export const toggleTodo = (id) => {
-    todos.update((currentTodos) => {
-        return currentTodos.map((todo) => {
-            if (todo.id === id) {
-                return {
-                    ...todo,
-                    complete: !todo.complete
-                };
-            }
-            return todo;
-        });
-    });
-}
+export const toggleComplete = (id) => {
+	todos.update((currentTodos) => {
+		return currentTodos.map((todo) => {
+			if (todo.id === id) {
+				return { ...todo, complete: !todo.complete };
+			}
+			return todo;
+		});
+	});
+};
 
-//editTodo function
 export const editTodo = (id, text) => {
-    todos.update((currentTodos) => {
-        return currentTodos.map((todo) => {
-            if (todo.id === id) {
-                return {
-                    ...todo,
-                    text
-                };
-            }
-            return todo;
-        });
-    });
-}
+	todos.update((currentTodos) => {
+		return currentTodos.map((todo) => {
+			if (todo.id === id) {
+				return { ...todo, text };
+			}
+			return todo;
+		});
+	});
+};
